32ed3af78561a3065f14832b84ee2d96ca92bd38
OblivionSan <oblivionsan@outlook.com>
2018-01-15 23:16:35 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8" />
+      <title>random-image-api</title>
+      <?php require_once './assets/api/ri-api.php'; ?>
+   </head>
+   <body>
+      <div>
+         <img src="<?php echo $path . $img ?>"/>
+      </div>
+   </body>
+</html>
